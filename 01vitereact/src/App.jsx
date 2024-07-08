8d9c8cefe67d94dda65cedb098f27db744290b10
jyotijais <jyotijais204@gmail.com>
2024-07-08 20:35:49 +0530

    interview

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from './chai.jsx'
+
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+  <>
+  <Chai/>
+  <h1>chai aur react {username}</h1>
+  <p>Test Para</p>
+  </>
+  )
+}
+
+export default App
