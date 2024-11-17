ad364401560a6f42e7ec3dee932651461550720c
jyoti <jyotijais204@gmail.com>
2024-11-17 16:43:10 +0530

    Mega project of complex blog

---
+++ 12MegaBlog/src/main.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {Provider} from 'react-redux'
+import store from './store/store.js'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import {AuthLayout, Login} from './components/index.js'
+
+
+import AddPost from './pages/AddPost';
+import Signup from './pages/Signup';
+import EditPost from './pages/EditPost';
+import AllPosts from './pages/AllPosts';
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  children: [
+    {
+      path: '/',
+      element: <Home/>,
+
+    },
+    {
+      path: '/login',
+      element: (
+        <AuthLayout authentication={false}>
+          <Login/>
+        </AuthLayout>
+      )
+    },
+    {
+      path: 'signup',
+      element: (
+        <AuthLayout>
+          <Signup />
+        </AuthLayout>
+      )
+    },
+    {
+      path: '/all-posts',
+      element: (
+        <AuthLayout authentication>
+          {" "}
+          <AllPosts />
+        </AuthLayout>
+      )
+    },
+    {
+      path: "/all-posts",
+      element: (
+        <AuthLayout authentication>
+          {" "}
+          <AddPost/>
+        </AuthLayout>
+      )
+    },
+    {
+      path: "/edit-post/:slug",
+      element: (
+       <AuthLayout authenticaton>
+        {" "}
+        <EditPost /> 
+       </AuthLayout>
+      )
+    },
+    
+  ],
+},
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
+  </React.StrictMode>,
+)
